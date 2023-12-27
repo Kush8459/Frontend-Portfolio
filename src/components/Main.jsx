768d745ff08bba9be73b038bd577509d9b6010f4
Kush8459 <agrawalkush04@gmail.com>
2023-12-27 23:54:32 +0530

    Created Contact section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,12 +3,22 @@ import React from "react";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { AiOutlineMail } from "react-icons/ai";
+import Image from "next/image";
 
 const Main = () => {
   return (
     <div className="w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-        <div>
+      <div className="max-w-[1240px] w-full h-full md:grid grid-cols-3 m-auto p-4 flex justify-center items-center">
+        <div className="w-auto h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+          <Image
+            className="rounded-xl"
+            src="/profile.jpg"
+            alt="/"
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className="col-span-2">
           <p className="uppercase text-sm tracking-widest text-gray-600">
             LET'S BUILD SOMETHING TOGETHER
           </p>
